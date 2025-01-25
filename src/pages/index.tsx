5fdd69f5bcad62948fd322d0d025fc2b7de9826b
Jiaheng Pan <jiahengpan12.15@gmail.com>
2025-01-25 18:56:43 -0500

    react component lab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-// import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Gallery from './qcomps/gallery_props'
+import List from './qcomps/list_keys_id'
+import RecipeList from './qcomps/recipes'
 
 export default function Home() {
   return (
-    <Profile />
+    <div>
+
+      {/* <div className={styles.main}>
+          <Gallery />
+      </div> */}
+
+      {/* <div className={styles.main}>
+      <List />
+      </div> */}
+      
+      <div className={styles.main}>
+      <RecipeList />
+      </div>
+
+    </div>
   )
 }
--- src/pages/qcomps/list_keys_id.tsx
+++ src/pages/qcomps/list_keys_id.tsx
@@ -1,24 +1,42 @@
-export const people = [{
-  id: 0,
-  name: 'Creola Katherine Johnson',
-  profession: 'mathematician',
-  accomplishment: 'spaceflight calculations',
-  imageId: 'szV5sdG'
-}, {
-  id: 1,
-  name: 'Mario José Molina-Pasquel Henríquez',
-  profession: 'chemist',
-  accomplishment: 'discovery of Arctic ozone hole',
-  imageId: 'YfeOqp2'
-}];
-
-function getImageUrl(imageId: string) {
-  return "https://i.imgur.com/" + imageId + "s.jpg"
-}
-export default function List() {
-  const listItems = people.map(person =>
-    <li>
-    </li>
-  );
-  return <ul>{listItems}</ul>;
-}
+interface Person {
+    id: number;
+    name: string;
+    profession: string;
+    accomplishment: string;
+    imageId: string;
+  }
+  
+  const people: Person[] = [{
+    id: 0,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'szV5sdG'
+  }, {
+    id: 1,
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+    accomplishment: 'discovery of Arctic ozone hole',
+    imageId: 'YfeOqp2'
+  }];
+  
+  function getImageUrl(imageId: string): string {
+      return `https://i.imgur.com/${imageId}s.jpg`;
+  }
+  
+  export default function List() {
+    const listItems = people.map(person => (
+      <li key={person.id}>
+        <img 
+          src={getImageUrl(person.imageId)} 
+          alt={person.name}
+        />
+        <p>
+          <b>{person.name}</b>
+          ({person.profession}) - {person.accomplishment}
+        </p>
+      </li>
+    ));
+  
+    return <ul>{listItems}</ul>;
+  }
